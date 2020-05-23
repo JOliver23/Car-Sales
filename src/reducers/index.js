5870d6e4134f75bce56f9504ddaedafaf9824dae
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-05-22 19:54:32 -0700

    install dependencies: redux/react-redux;added folders for actions reducers; provider and store implemented; initial state copied, skeleton reducer built for renderiiing purposes

---
+++ src/reducers/index.js
@@ -0,0 +1,30 @@
+import { UPDATE_ORDER } from '../actions';
+
+export const initialState = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+};
+
+export const appReducer = (state, action) => {
+    console.log("state", state, "action", action);
+    switch (action.type) {
+        case UPDATE_ORDER:
+            return {
+                state
+            }
+        default:
+            return state;
+    }
+}
